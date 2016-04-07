d749a965336cc20e5d8a8bc05c44b95f1212f711
fzed51 <fzed51+git@gmail.com>
2016-04-07 11:26:23 +0200

    Remise à plat de la classe Vue

--- src/Vue.php
+++ src/Vue.php
@@ -24,119 +24,14 @@
  * THE SOFTWARE.
  */
 
-namespace Core\Vue;
+namespace fzed51\Vue;
 
 /**
  * Description of Vue
  *
  * @author fabien.sanchez
  */
-class Vue implements IsRenderable {
-
-    use TraitVueLayout,
-        TraitVueModel,
-        TraitVueTitre,
-        TraitVueMeta,
-        TraitVueStyle,
-        TraitVueScript;
-
-    /**
-     * Conteneur des données de la vue
-     * @var array
-     */
-    protected $data = array();
-
-    /**
-     *
-     * @param string $modelSlug
-     * @param string $layoutSlug
-     */
-    public function __construct($modelSlug, $layoutSlug = '') {
-        $this->setModel($modelSlug);
-        if (!empty($layoutSlug)) {
-            $this->setLayout($layoutSlug);
-        } else {
-            $this->setLayout(self::$DefautLayout);
-        }
-    }
-
-    public function __unset($field) {
-        if ($this->dataExist($field)) {
-            unset($this->data[$field]);
-        }
-    }
-
-    private function dataExist($field) {
-        return isset($this->data[$field]);
-    }
-
-    public function __get($field) {
-        switch ($field) {
-            case 'titre':
-                return $this->renderTitre();
-            case 'meta':
-                return $this->renderMeta();
-            case 'style':
-                return $this->renderStyle();
-            case 'script':
-                return $this->renderScript();
-            default:
-                return $this->get($field);
-        }
-    }
-
-    public function __isset($field) {
-        return $this->dataExist($field);
-    }
-
-    public function __set($field, $value) {
-        $this->setData($field, $value);
-    }
-
-    public function setData($field, $value) {
-        $this->data[$field] = $value;
-    }
-
-    public function setDatas(array $datas) {
-        foreach ($datas as $field => $value) {
-            $this->setData($field, $value);
-        }
-    }
-
-    public function get($field, $default = "") {
-        if ($this->dataExist($field)) {
-            return $this->data[$field];
-        } else {
-            return $default;
-        }
-    }
-
-    private function renderFile($__file, array $__data) {
-        extract($__data);
-        unset($__data);
-        ob_start();
-        include $__file;
-        $contents = ob_get_contents();
-        ob_end_clean();
-        return $contents;
-    }
-
-    public function render(array $data = []) {
-        $data = array_merge($this->data, $data);
-        $this->content = $this->renderFile($this->getModelFile(), $data);
-        if (!empty($this->getLayout())) {
-            $this->content = $this->renderFile($this->getLayoutFile(), $data);
-        }
-        return $this->content;
-    }
-
-    public function widget($slug, array $data = array(), $name = '') {
-        $fileName = self::$DossierModel . "\\widget\\" . str_replace('.', "\\", $slug) . '.php';
-        if (!file_exists($fileName)) {
-            throw new VueException("Le widget '$slug' n'a pas été trouvé");
-        }
-        $data['widget_name'] = $name;
-        return $this->renderFile($fileName, $data);
-    }
+class Vue
+{
 
 }
